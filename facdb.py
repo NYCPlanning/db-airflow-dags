221d17060614de499af23432615daf288feb320d
Jonathan Pichot <jonathan@pichot.us>
2017-08-01 13:15:08 -0400

    facdb test

--- facdb.py
+++ facdb.py
@@ -8,21 +8,35 @@ seven_days_ago = datetime.combine(datetime.today() - timedelta(7),
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': seven_days_ago,
+    'start_date': datetime.today(),
     'email': ['jpichot@planning.nyc.gov'],
-    'email_on_failure': False,
+    'email_on_failure': True,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 5,
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
-    'simple',
+# Data Loading Scripts
+dag_data-loading-scripts = DAG(
+    'facdb-data-loading-scripts',
     default_args=default_args
 )
 
-t1 = BashOperator(
-    task_id='testairflow',
-    bash_command='cd ~/scripts/civic-data-loader/ && node loader get bic_facilities_tradewaste',
-    dag=dag
-)
+for db in ["facdb_datasources", "facdb_uid_key", "dcp_facilities_togeocode"]:
+    task = BashOperator(
+        task_id='data-loader_' + db,
+        bash_command="npm run get {0} --prefix=~/scripts/data-loader-scripts".format(db),
+        dag=dag)
+
+
+# FacDB controller dag
+# dag_facdb = DAG(
+#     'facdb',
+#     default_args=default_args
+# )
+#
+# t1 = BashOperator(
+#     task_id='testairflow',
+#     bash_command='cd ~/scripts/civic-data-loader/ && node loader get bic_facilities_tradewaste',
+#     dag=dag
+# )
