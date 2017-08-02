bc63391ad9f0b126860ae7957b2b1ff28b8d146f
Jonathan Pichot <jonathan@pichot.us>
2017-08-02 12:35:14 -0400

    add data sources

--- data_sources.py
+++ data_sources.py
@@ -3,39 +3,42 @@ facdb = [
     "facdb_uid_key",
     "dcp_facilities_togeocode",
 
-    # Data Sources
+    # DCP Data Sources
     "dcp_mappluto",
     "dcp_boroboundaries_wi",
     "dcp_cdboundaries",
     "dcp_censustracts",
     "dcp_councildistricts",
     "dcp_ntaboundaries",
-    "doitt_zipcodes"
+    "doitt_zipcodes",
+
+    # Outside Datasets
+    "bic_facilities_tradewaste",
+    "dca_facilities_operatingbusinesses",
+    "dcla_facilities_culturalinstitutions",
+    "dcp_facilities_sfpsd",
+    "dcp_facilities_pops",
+    "dfta_facilities_contracts",
+    "doe_facilities_busroutesgarages",
+    "doe_facilities_universalprek",
+    "dohmh_facilities_daycare",
+    "dpr_parksproperties",
+    "hhc_facilities_hospitals",
+    "nycha_facilities_policeservice",
+    "nysdec_facilities_lands",
+    "nysdec_facilities_solidwaste",
+    "nysdoh_facilities_healthfacilities",
+    "nysdoh_nursinghomebedcensus",
+    "nysomh_facilities_mentalhealth",
+    "nysopwdd_facilities_providers",
+    "nysparks_facilities_historicplaces",
+    "nysparks_facilities_parks",
+    "usdot_facilities_airports",
+    "usdot_facilities_ports",
+    "usnps_facilities_parks",
 ]
 
-# bic_facilities_tradewaste
-# dca_facilities_operatingbusinesses
-# dcla_facilities_culturalinstitutions
-# dcp_facilities_sfpsd
-# dcp_facilities_pops
-# dfta_facilities_contracts
-# doe_facilities_busroutesgarages
-# doe_facilities_universalprek
-# dohmh_facilities_daycare
-# dpr_parksproperties
-# hhc_facilities_hospitals
-# nycha_facilities_policeservice
-# nysdec_facilities_lands
-# nysdec_facilities_solidwaste
-# nysdoh_facilities_healthfacilities
-# nysdoh_nursinghomebedcensus
-# nysomh_facilities_mentalhealth
-# nysopwdd_facilities_providers
-# nysparks_facilities_historicplaces
-# nysparks_facilities_parks
-# usdot_facilities_airports
-# usdot_facilities_ports
-# usnps_facilities_parks
+
 #
 # acs_facilities_daycareheadstart
 # dcas_facilities_colp
